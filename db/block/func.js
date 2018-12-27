100ac3281a75b9c3ad8c2ef51f27640e5a8cae3c
DESKTOP-FSI8QAQ\Nguyen Thinh Khang <thinhkhang97@gmail.com>
2018-12-27 09:47:21 +0700

    read multy transaction in block

--- db/block/func.js
+++ db/block/func.js
@@ -3,7 +3,7 @@ let client = RpcClient('wss://zebra.forest.network:443');
 import Tx from '../../transaction';
 const base32 = require('base32.js');
 import {
-    createAccount, 
+    createAccount,
     getAccountInfo,
     updateFollowingsAccount,
     updateImageAccount,
@@ -12,8 +12,8 @@ import {
     addTimeline,
     updateSequenceAccount
 } from '../fn/account';
-import {payment} from '../fn/balance';
-import {createPost, } from '../fn/newsfeed';
+import { payment } from '../fn/balance';
+import { createPost, } from '../fn/newsfeed';
 const vstruct = require('varstruct');
 const PlainTextContent = vstruct([
     { name: 'type', type: vstruct.UInt8 },
@@ -21,102 +21,106 @@ const PlainTextContent = vstruct([
 ]);
 const Followings = vstruct([
     { name: 'addresses', type: vstruct.VarArray(vstruct.UInt16BE, vstruct.Buffer(35)) },
-  ]);
+]);
 
 // Read the data in block and import to data base
 export const processBlockData = async (height) => {
     const tx = await getTransactionInBlock(height);
     if (tx != null) {
-        console.log('PROCESS HEIGHT:',height);
-        const data = decodeData(tx.data)
-        const account = data.account;
-        const operation = data.operation;
-        switch (operation) {
-            case 'create_account':
-                const newAccount = data.params.address;
-                const isExistNewAccount = await getAccountInfo(newAccount);
-                if (isExistNewAccount != null) {
-                    console.log('Account is already exist');
-                    return;
-                }
-                console.log( 'ACCOUNT', account,'CREATE ACCOUNT', newAccount, 'SEQUENCE', 0, 'BALANCE', 0, 'POSTS', []);
-                await createAccount(account,newAccount,data.sequence);
-                await addTimeline(account,`Create account ${newAccount}`,tx.time);
-                await addTimeline(newAccount,`Created by ${account}`,tx.time);
-                await updateSequenceAccount(account,data.sequence);
-                return;
-            case 'payment':
-                const toAccount = data.params.address;
-                const isExistPayment = await getAccountInfo(toAccount);
-                if (isExistPayment === null) {
-                    console.log('Account payment isnt exist');
-                    return;
-                }
-                // find the to address
-                const amount = parseInt(data.params.amount);
-                console.log('ACCOUNT PAYMENT: FROM', account, 'TO', toAccount, 'AMOUNT', amount);
-                const d = await payment(account, 0 - amount, data.sequence);
-                await payment(toAccount, amount, null);
-                await addTimeline(account,`Payment ${toAccount} with amount: ${amount}`,tx.time);
-                await addTimeline(toAccount,`Received ${amount} from  ${account}`,tx.time);
-                await updateSequenceAccount(account,data.sequence);
-                return d;
-            case 'post':
-                try {
-                    const content = PlainTextContent.decode(data.params.content)
-                    const keys = data.params.keys;
-                    const shareWith = [];
-                    keys.map(key => {
-                        shareWith.push({
-                            publicKey: key.toString('base64')
-                        });
-                    })
-                    console.log('POST: FROM', account, 'CONTENT', content, 'SHARE WITH', shareWith);
-                    await createPost(account, 'No title',content.text,tx.time,shareWith, data.sequence);
-                    await addTimeline(account,`Post a content: ${content.text}`,tx.time);
-                    await updateSequenceAccount(account,data.sequence);
-                } catch(err) {
-                    console.log('ERROR TO READ BLOCK');
-                }
-                return;
-            case 'update_account': 
-                try {
-                    switch (data.params.key) {
-                        case 'name':
-                            const name = data.params.value.toString();
-                            await updateNameAccount(account,name,data.sequence);
-                            console.log('UPDATE ACCOUNT', account, 'NAME', name);
-                            await addTimeline(account,`Update name to ${name}`,tx.time);
-                            await updateSequenceAccount(account,data.sequence);
-                                return;
-                        case 'picture':
-                            const imgData = data.params.value.toString('base64');
-                            await updateImageAccount(account,imgData,data.sequence);
-                            console.log('UPDATE ACCOUNT', account, 'IMAGE');
-                            await addTimeline(account,`Update picture`,tx.time);
-                            await updateSequenceAccount(account,data.sequence);
-                                return;
-                        case 'followings':
-                            const listFollowing = Followings.decode(data.params.value).addresses;
-                            const followingsData = listFollowing.map(f=>{
-                                return {
-                                    publicKey: base32.encode(f)
-                                }
-                            })
-                            console.log('UPDATE ACCOUNT', account, 'FOLLOWING', followingsData);
-                            await updateFollowingsAccount(account,followingsData,data.sequence);
-                            await updateSequenceAccount(account,data.sequence);
-                            await addTimeline(account,`Following ${listFollowing[listFollowing.length-1]}`,tx.time);
-                            return;
+        console.log('PROCESS HEIGHT:', height);
+        const txs = tx.data; // array tx
+        for (let j = 0; j < txs.length; j++) {
+            const data = decodeData(txs[j]);; //decode
+            const account = data.account;
+            const operation = data.operation;
+            switch (operation) {
+                case 'create_account':
+                    const newAccount = data.params.address;
+                    const isExistNewAccount = await getAccountInfo(newAccount);
+                    if (isExistNewAccount != null) {
+                        console.log('Account is already exist');
+                        return;
                     }
-                }catch(err){
-                    console.log('ERROR READING UPDATE ACCOUNT BLOCK')
-                }
-                return;
-            case 'interact':
-                console.log('INTERACT');
-                await updateSequenceAccount(account,data.sequence);
-                break;
+                    console.log('ACCOUNT', account, 'CREATE ACCOUNT', newAccount, 'SEQUENCE', 0, 'BALANCE', 0, 'POSTS', []);
+                    await createAccount(account, newAccount, data.sequence);
+                    await addTimeline(account, `Create account ${newAccount}`, tx.time);
+                    await addTimeline(newAccount, `Created by ${account}`, tx.time);
+                    await updateSequenceAccount(account, data.sequence);
+                    return;
+                case 'payment':
+                    const toAccount = data.params.address;
+                    const isExistPayment = await getAccountInfo(toAccount);
+                    if (isExistPayment === null) {
+                        console.log('Account payment isnt exist');
+                        return;
+                    }
+                    // find the to address
+                    const amount = parseInt(data.params.amount);
+                    console.log('ACCOUNT PAYMENT: FROM', account, 'TO', toAccount, 'AMOUNT', amount);
+                    const d = await payment(account, 0 - amount, data.sequence);
+                    await payment(toAccount, amount, null);
+                    await addTimeline(account, `Payment ${toAccount} with amount: ${amount}`, tx.time);
+                    await addTimeline(toAccount, `Received ${amount} from  ${account}`, tx.time);
+                    await updateSequenceAccount(account, data.sequence);
+                    return d;
+                case 'post':
+                    try {
+                        const content = PlainTextContent.decode(data.params.content)
+                        const keys = data.params.keys;
+                        const shareWith = [];
+                        keys.map(key => {
+                            shareWith.push({
+                                publicKey: key.toString('base64')
+                            });
+                        })
+                        console.log('POST: FROM', account, 'CONTENT', content, 'SHARE WITH', shareWith);
+                        await createPost(account, 'No title', content.text, tx.time, shareWith, data.sequence);
+                        await addTimeline(account, `Post a content: ${content.text}`, tx.time);
+                        await updateSequenceAccount(account, data.sequence);
+                    } catch (err) {
+                        console.log('ERROR TO READ BLOCK');
+                    }
+                    return;
+                case 'update_account':
+                    try {
+                        switch (data.params.key) {
+                            case 'name':
+                                const name = data.params.value.toString();
+                                await updateNameAccount(account, name, data.sequence);
+                                console.log('UPDATE ACCOUNT', account, 'NAME', name);
+                                await addTimeline(account, `Update name to ${name}`, tx.time);
+                                await updateSequenceAccount(account, data.sequence);
+                                return;
+                            case 'picture':
+                                const imgData = data.params.value.toString('base64');
+                                await updateImageAccount(account, imgData, data.sequence);
+                                console.log('UPDATE ACCOUNT', account, 'IMAGE');
+                                await addTimeline(account, `Update picture`, tx.time);
+                                await updateSequenceAccount(account, data.sequence);
+                                return;
+                            case 'followings':
+                                const listFollowing = Followings.decode(data.params.value).addresses;
+                                const followingsData = listFollowing.map(f => {
+                                    return {
+                                        publicKey: base32.encode(f)
+                                    }
+                                })
+                                console.log('UPDATE ACCOUNT', account, 'FOLLOWING', followingsData);
+                                await updateFollowingsAccount(account, followingsData, data.sequence);
+                                await updateSequenceAccount(account, data.sequence);
+                                await addTimeline(account, `Following ${listFollowing[listFollowing.length - 1]}`, tx.time);
+                                return;
+                        }
+                    } catch (err) {
+                        console.log('ERROR READING UPDATE ACCOUNT BLOCK')
+                    }
+                    return;
+                case 'interact':
+                    console.log('INTERACT');
+                    await updateSequenceAccount(account, data.sequence);
+                    break;
+            }
+
         }
     }
 }
@@ -150,7 +154,7 @@ export function getTransactionInBlock(height) {
         if (block.header.num_txs > 0) {
             const txinfo = {
                 time: block.header.time,
-                data: block.data.txs[0]
+                data: block.data.txs
             }
             console.log(txinfo);
             return txinfo;
